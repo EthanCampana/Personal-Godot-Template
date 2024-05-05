dad923cc84a9460c86fc87095d1725ad3c4dfdcb
Ethan <ethancampana@gmail.com>
2024-05-04 21:27:43 -0400

    Fix some character controller bugs

--- Scripts/States/StateMachine.gd
+++ Scripts/States/StateMachine.gd
@@ -15,17 +15,16 @@ func _ready():
     for child in get_children():
         if child is State:
             states[child.name.to_lower()] = child
-    states[current_state.to_lower()].Enter({})
 
 # Calls the related physics process function of the current state.
 func _process(delta: float):
     if current_state:
-        current_state._process(delta)
+        current_state._update(delta)
 
 # Calls the related physics process function of the current state.
 func _physics_process(delta: float):
     if current_state:
-        current_state._physics_process(delta)
+        current_state._physics_update(delta)
 
 # Calls when the state machine changes the state.
 func change_state(state_name: String, msg: Dictionary):
